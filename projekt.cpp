77c01b11341ea35b782f3b29043afbcd9ec43d1b
skazzel <48695058+skazzel@users.noreply.github.com>
2020-12-06 19:36:27 +0100

    Add files via upload

--- projekt.cpp
+++ projekt.cpp
@@ -4,12 +4,6 @@
 #include <string.h>
 #include <iostream>
 
-#define SIMULATION_DAYS 365
-#define PRUPLAV_RYCHLE 10
-#define PRUPLAV_POMALU_A 14
-#define PRUPLAV_POMALU_B 18
-
-#define PROFIT 0
 #define ZBOZI_TRIDA_1 0.81 // podil zbozi 10%
 #define ZBOZI_TRIDA_2 0.81 // podil zbozi 10%
 #define ZBOZI_TRIDA_3 0.68 // podil zbozi 15%
@@ -21,53 +15,72 @@ class generator: public Event {
 public:
 	void Behavior();
 };
-class lod: public Process {
-public:
-	void Behavior();
-};
 
+int profit = 0;
+int simulation_time = 0;
+Histogram pozadavky_lode("Pozadavky lode", 0, 1000, 20);
 int pocet_lodi = 40;
+int STREDNI_LOZENI_PLAVIDLA = 3088; 
+int ZBOZI_TRIDA_cena = 0;
+const int MINUTE = 60;
+const int HOUR = 3600;
+const int DAY = 86400;
+const int WEEK = 604800;
 Store Plavebni_komora("Plavebni_komora", 1);
 
 void print_help() {
 	_Print("pomoc is here\n");
 }
 
-void lod::Behavior() {
-	if (!Plavebni_komora.Full()) {
-		Enter(Plavebni_komora);
-		double random_cislo = Random();
-		_Print("random %d\n", random_cislo);
-		if (random_cislo < 0.1) {
-			Wait(Uniform(10, 14)); // cekame nez lod proplave
-		}
-		else if (random_cislo < 0.2 && random_cislo > 0.1) {
-			Wait(Uniform(PRUPLAV_RYCHLE, PRUPLAV_POMALU_B)); // cekame nez lod proplave
-		}
-		else if (random_cislo < 0.35 && random_cislo > 0.2) {
-			Wait(Uniform(PRUPLAV_RYCHLE, PRUPLAV_POMALU_B)); // cekame nez lod proplave
-		}
-		else if (random_cislo < 0.5 && random_cislo > 0.35) {
-			Wait(Uniform(PRUPLAV_RYCHLE, PRUPLAV_POMALU_A)); // cekame nez lod proplave
-		}
-		else if (random_cislo < 0.7 && random_cislo > 0.5) {
-			Wait(Uniform(PRUPLAV_RYCHLE, PRUPLAV_POMALU_A)); // cekame nez lod proplave
-		}
-		else if (random_cislo < 1.0 && random_cislo > 0.7) {
-			Wait(Uniform(PRUPLAV_RYCHLE, PRUPLAV_POMALU_A)); // cekame nez lod proplave
-		}
-		Leave(Plavebni_komora);
-	}
-	else {
-		Wait(15);
-	}
+int get_time() {
+	return (int)(Time)%(DAY);
 }
 
-void generator::Behavior() {
-	_Print("generator\n");
-	for (int i = 0; i < pocet_lodi; i++) {
-		(new lod)->Activate();
+class lod: public Process {
+	double prichod;
+	void Behavior() {
+		prichod = Time;
+		int time_of_day = get_time();
+		if (!Plavebni_komora.Full()) {
+			Enter(Plavebni_komora);
+			double random_cislo = Random();
+			_Print("random %d\n", random_cislo);
+			if (random_cislo < 0.1) {
+				Wait(Exponential(10*MINUTE)); // cekame nez lod proplave
+				ZBOZI_TRIDA_cena = 0.81;
+			}
+			else if (random_cislo < 0.2 && random_cislo > 0.1) {
+				Wait(Exponential(10*MINUTE)); // cekame nez lod proplave
+				ZBOZI_TRIDA_cena = 0.81;
+			}
+			else if (random_cislo < 0.35 && random_cislo > 0.2) {
+				Wait(Exponential(8*MINUTE)); // cekame nez lod proplave
+				ZBOZI_TRIDA_cena = 0.68;
+			}
+			else if (random_cislo < 0.5 && random_cislo > 0.35) {
+				Wait(Exponential(7*MINUTE)); // cekame nez lod proplave
+				ZBOZI_TRIDA_cena = 0.66;
+			}
+			else if (random_cislo < 0.7 && random_cislo > 0.5) {
+				Wait(Exponential(5*MINUTE)); // cekame nez lod proplave
+				ZBOZI_TRIDA_cena = 0.57;
+			}
+			else if (random_cislo < 1.0 && random_cislo > 0.7) {
+				Wait(Exponential(4*MINUTE)); // cekame nez lod proplave
+				ZBOZI_TRIDA_cena = 0.40;
+			}
+			profit = STREDNI_LOZENI_PLAVIDLA * ZBOZI_TRIDA_cena;
+			_Print("Profit %d\n", profit);
+			Leave(Plavebni_komora);
+			pozadavky_lode(Time - prichod);
+		}
 	}
+};
+
+void generator::Behavior() {
+	(new lod)->Activate();
+
+	//Activate(Time + Exponential(1));
 }
 
 int main(int argc, char const *argv[])
@@ -75,6 +88,7 @@ int main(int argc, char const *argv[])
 
 	SetOutput("data.dat");
 	_Print("# IMS - DOL projekt\n");
+	simulation_time = WEEK;
 
 	for (int i = 1; i < argc; i++) {
 		if(strcmp(argv[i], "--help") == 0){
@@ -85,10 +99,19 @@ int main(int argc, char const *argv[])
 			pocet_lodi = atol(argv[i+1]);
 			_Print("Pocet lodi %d\n", pocet_lodi);
 		}
+		if (strcmp(argv[i], "-t") == 0) {
+			try {
+				simulation_time = std::stoi(argv[i+1]);
+			}
+			catch (std::exception const &e) {
+				_Print("neco se nepovedlo");
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
-
+	_Print("%d", simulation_time);
 	RandomSeed(time(NULL));
-	Init(0, SIMULATION_DAYS);
+	Init(0, simulation_time);
 	(new generator)->Activate();
 	Run();
 
