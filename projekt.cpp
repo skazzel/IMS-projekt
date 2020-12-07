d150ee5bb4100bbb395e8c0b65df34ed6d3d5f9c
skazzel <48695058+skazzel@users.noreply.github.com>
2020-12-07 16:57:56 +0100

    Add files via upload

--- projekt.cpp
+++ projekt.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <cstring>
 #include <iostream>
+#include <vector>
 
 #define ZBOZI_TRIDA_1 0.81 // podil zbozi 10%
 #define ZBOZI_TRIDA_2 0.81 // podil zbozi 10%
@@ -16,16 +17,17 @@ public:
 	void Behavior();
 };
 
-double profit = 0;
+long long int profit = 0;
+std::vector<long long int> soucet_profit;
 int simulation_time = 0;
-Histogram pozadavky_lode("Pozadavky lode", 0, 1000, 20);
-int pocet_lodi = 40;
+int pocet_lodi = 5000;
 int STREDNI_LOZENI_PLAVIDLA = 3088; 
 float ZBOZI_TRIDA_cena = 0;
 const int MINUTE = 60;
 const int HOUR = 3600;
 const int DAY = 86400;
 const int WEEK = 604800;
+int YEAR_SECOND = DAY*365;
 //koridor
 enum Corridor_type {
     DOL,
@@ -38,6 +40,9 @@ Corridor_type corridor = DOL;
 int year = 1;
 //celkove naklady
 double costs;
+int pocet_dnu = 1;
+//pocet komor
+int chamber_number = 30;
 
 Store Plavebni_komora("Plavebni_komora", 1);
 
@@ -50,11 +55,7 @@ void print_help() {
     _Print("-n, --number        znaci pocet lodi ktere propluji danym koridorem za dany cas\n");
     _Print("-y, --year          pocet let, pro ktery se pocitat\n");
     _Print("--------------------------------------------------------------------------------\n");
-    _Print("Bez specifikovani hodnot jsou hodnoty nastavene na -c DOL, -n 40 a -y 1\n");
-}
-
-int get_time() {
-	return (int)(Time)%(DAY);
+    _Print("Bez specifikovani hodnot jsou hodnoty nastavene na -c DOL, -n 5000 a -y 1\n");
 }
 
 void get_cost(){
@@ -93,91 +94,88 @@ void get_cost(){
     costs = (personal_costs + operating_costs) * year;
 }
 
+void get_chamber() {
+    switch (corridor){
+    case DOL:
+        chamber_number = 30;
+        break;
+    case DO:
+        chamber_number = 19;
+        break;
+    case DL:
+        chamber_number = 19;
+        break;
+    case LO:
+        chamber_number = 19;
+        break;
+    default:
+        break;
+    }
+}
+
 class Lod: public Process {
-	int pracovni_doba[3];
 public:
-	Lod(int doba1, int doba2, int doba3) : Process() {
-		pracovni_doba[0] = doba1;
-		pracovni_doba[1] = doba2;
-		pracovni_doba[2] = doba3;
-	}
-
-	double prichod;
-
 	void Behavior() {
-		prichod = Time;
-		int time_of_day = get_time();
-		//Wait(10);
-		//_Print("TIME %f\n", Time);
 		if (!Plavebni_komora.Full()) {
 			Wait(Exponential(20*MINUTE));
 			Enter(Plavebni_komora, 1);
 			double random_cislo = Random();
 			if (random_cislo < 0.1) {
 				Wait(Exponential(10*MINUTE)); // cekame nez lod proplave
-				ZBOZI_TRIDA_cena = 0.81;
+				ZBOZI_TRIDA_cena = ZBOZI_TRIDA_1;
 			}
 			else if (random_cislo < 0.2 && random_cislo > 0.1) {
 				Wait(Exponential(10*MINUTE)); // cekame nez lod proplave
-				ZBOZI_TRIDA_cena = 0.81;
+				ZBOZI_TRIDA_cena = ZBOZI_TRIDA_2;
 			}
 			else if (random_cislo < 0.35 && random_cislo > 0.2) {
 				Wait(Exponential(8*MINUTE)); // cekame nez lod proplave
-				ZBOZI_TRIDA_cena = 0.68;
+				ZBOZI_TRIDA_cena = ZBOZI_TRIDA_3;
 			}
 			else if (random_cislo < 0.5 && random_cislo > 0.35) {
 				Wait(Exponential(7*MINUTE)); // cekame nez lod proplave
-				ZBOZI_TRIDA_cena = 0.66;
+				ZBOZI_TRIDA_cena = ZBOZI_TRIDA_4;
 			}
 			else if (random_cislo < 0.7 && random_cislo > 0.5) {
 				Wait(Exponential(5*MINUTE)); // cekame nez lod proplave
-				ZBOZI_TRIDA_cena = 0.57;
+				ZBOZI_TRIDA_cena = ZBOZI_TRIDA_5;
 			}
 			else if (random_cislo < 1.0 && random_cislo > 0.7) {
 				Wait(Exponential(4*MINUTE)); // cekame nez lod proplave
-				ZBOZI_TRIDA_cena = 0.40;
+				ZBOZI_TRIDA_cena = ZBOZI_TRIDA_6;
 			}
-			profit = STREDNI_LOZENI_PLAVIDLA * ZBOZI_TRIDA_cena;
-			double random_procento = Random();
-			if (random_procento <= 0.33) {
-				
+			if (Time > (86400*pocet_dnu)) {
+				pocet_dnu++;
 			}
-			_Print("Profit %lf\n", profit);
-			//_Print("CENA %f\n", ZBOZI_TRIDA_cena);
-			//_Print("plavebni komora11 %d\n", Plavebni_komora.Full());
-			_Print("TIME %f\n", Time);
+			if (Time >= 0) {
+				double cas = Time / (86400 * pocet_dnu);
+				cas *= 24;
+				if (cas >= 6 && cas <= 19) {
+					soucet_profit.push_back(STREDNI_LOZENI_PLAVIDLA * ZBOZI_TRIDA_cena * chamber_number);
+				}
+				else if (cas > 19 && cas <= 24) {
+					soucet_profit.push_back(chamber_number * STREDNI_LOZENI_PLAVIDLA * ZBOZI_TRIDA_cena + 25);
+				}
+				else if (cas < 6) {
+					soucet_profit.push_back(chamber_number * STREDNI_LOZENI_PLAVIDLA * ZBOZI_TRIDA_cena + 60);
+				}
+			}
 			Leave(Plavebni_komora, 1);
-			//pozadavky_lode(Time - prichod);
 		}
 	}
 };
 
 void generator::Behavior() {
-	_Print("Pocet lodi %d\n", pocet_lodi);
 	for (int i = 0; i < pocet_lodi; i++) {
-		Lod* lod = (new Lod(6, 16, 19));
-		lod->Activate();
+		(new Lod)->Activate();
 	}
-	/*int tod = get_time();
-	_Print("CAS %d\n", tod);
-	double timeToNextRequest = 0;
-		if(tod < (12*HOUR)){
-			timeToNextRequest = 2057;
-		}else if(tod < (18*HOUR)){
-			timeToNextRequest = 280;
-		}else if(tod < (22*HOUR)){
-			timeToNextRequest = 305;
-		}else{
-			timeToNextRequest = 500;
-		}
-		Activate(Time+Normal(timeToNextRequest, 0.1));*/
 }
 
 int main(int argc, char const *argv[])
 {
 	SetOutput("data.dat");
 	_Print("# IMS - DOL projekt\n");
-	simulation_time = WEEK;
+	simulation_time = YEAR_SECOND;
 
 	for (int i = 1; i < argc; i++) {
         //vypis napovedy
@@ -200,15 +198,6 @@ int main(int argc, char const *argv[])
                     exit(EXIT_FAILURE);
                 }
             }
-		}
-		if(strcmp(argv[i], "-t") == 0) {
-			try {
-				simulation_time = std::stoi(argv[i+1]);
-			}
-			catch (std::exception const &e) {
-				_Print("neco se nepovedlo");
-				exit(EXIT_FAILURE);
-			}
 		}
 		//vyber koridoru
 		if((strcmp(argv[i], "--corridor") == 0) || (strcmp(argv[i], "-c") == 0)){
@@ -242,6 +231,7 @@ int main(int argc, char const *argv[])
             else {
                 try {
 				    year = std::stoi(argv[i]);
+				    simulation_time = YEAR_SECOND * year;
                 }
                 catch (std::exception const &e) {
                     std::cerr << "Rok musi byt cele cislo" << std::endl;
@@ -250,14 +240,27 @@ int main(int argc, char const *argv[])
             }
 		}
 	}
+	get_chamber();
     get_cost();
-    _Print("Celkove naklady: %.0lf Kc\n", costs);
-	_Print("%d \n", simulation_time);
 	RandomSeed(time(NULL));
 	Init(0, simulation_time);
 
 	(new generator)->Activate();
 	Run();
+
+	for (long long int x : soucet_profit) {
+		profit += x;
+	}
+	_Print("Celkove naklady: %.0lf Kc\n", costs);
+	_Print("Vynosy z lodni prepravy: %lld Kc\n", profit);
+	if ((profit-costs) > 0) {
+		_Print("Celkovy vydelek: %.2lf Kc\n", profit - costs);
+		_Print("Celkovy vydelek v mil: %.2lf Mil. Kc\n", (profit - costs)/1000000);
+	}
+	else {
+		_Print("Celkova ztrata: %.2lf Kc\n", profit - costs);
+		_Print("Celkova ztrata v mil: %.2lf Mil. Kc\n", (profit - costs)/1000000);
+	}
 
 	return 0;
 }
